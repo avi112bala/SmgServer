534ae4f9339601ada030a0d299f4378f9e74b6ed
Avi112bala <avinash20802bala@gmail.com>
2025-09-07 12:04:10 +0530

    srverdeploy

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://sheildmotorgroup_db_user:Shieldmotor@1234@cluster0.btdezfb.mongodb.net/SMG?retryWrites=true&w=majority",
+    await mongoose.connect(
+      "mongodb+srv://sheildmotorgroup_db_user:Smg1234@cluster0.btdezfb.mongodb.net/SMG?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
